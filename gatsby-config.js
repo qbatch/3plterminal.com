94f286e139bed833423bf817e53e3add760aecf5
kalash-qbatch <105213530+kalash-qbatch@users.noreply.github.com>
2024-06-26 17:31:19 +0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,34 @@ module.exports = {
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  }],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "YOUR_GOOGLE_ANALYTICS_TRACKING_ID", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object is used for configuration specific to this plugin
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to the gtag library
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
+    // other plugins
+  ],
 };
