07337edd390c77b9e35e7a033c52a958e388abc8
kalash-qbatch <105213530+kalash-qbatch@users.noreply.github.com>
2024-11-05 14:23:55 +0500

    Updates

---
+++ 350-12c1df9fa452727e8010.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunk_3pl_landing_page=self.webpackChunk_3pl_landing_page||[]).push([[350],{8350:function(e,a,t){t.r(a),t.d(a,{default:function(){return m}});var n=t(6540),i=t(8396),l=t.n(i),r=t.p+"static/button-2-c380bb7e1f211c337981b3218a789742.svg",o=t.p+"static/no-more-lose-heading-ab9ec0f8a11e3affc9b4705a1e007170.svg",c=t(7700),p=t(7581),s=t.p+"static/no-more-delay-a2290912b9ac28b8d122c31e939a9f2a.svg",d=t(641);const g=p.default.div.withConfig({displayName:"style__NoMoreDelaysWrapper",componentId:"sc-1m7hyvy-0"})(["background-image:url(",");background-position:right;background-repeat:no-repeat;background-size:cover;padding:52px 0px 47px;"," "," "," .banner{min-height:313px;display:flex;flex-direction:column;align-items:flex-start;justify-content:center;","}img.heading{margin-bottom:12px;max-width:100%;}p{color:#fff;margin:0;font-weight:100;font-size:24px;","}a{img{padding-top:60px;","}}"],s,d.$.sm`
+margin-top:75px;
+`,d.$.lg`
+  padding: 52px 0 47px;
+`,d.$.sm`
+  padding: 52px 0 47px;
+`,d.$.sm`
+min-height:auto;
+`,d.$.sm`
+    font-size:16px;
+    `,d.$.lg`
+  max-width:100%;
+`);var m=()=>n.createElement(g,null,n.createElement(c.default,null,n.createElement(l(),{animateIn:"slideInLeft",delay:200},n.createElement("div",{className:"banner"},n.createElement("img",{className:"heading",src:o,alt:"Heading",title:"Heading"}),n.createElement("p",null,"Ensure a smooth, profitable journey without"),n.createElement("p",null,"sudden chargebacks and disruptions."),n.createElement("a",{href:"#"},n.createElement("img",{src:r,alt:"Button",title:"Button"})," ")))))}}]);
+//# sourceMappingURL=350-12c1df9fa452727e8010.js.map
