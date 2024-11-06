f662f63b97acc694241082a50ae6f334cad7fa43
kalash-qbatch <105213530+kalash-qbatch@users.noreply.github.com>
2024-11-06 16:08:44 +0500

    Updates

---
+++ 534-8446876ddfb820d8d8c9.js
@@ -0,0 +1,27 @@
+"use strict";(self.webpackChunk_3pl_landing_page=self.webpackChunk_3pl_landing_page||[]).push([[534],{534:function(e,t,n){n.r(t),n.d(t,{default:function(){return p}});var a=n(6540),i=n(1521),l=n(8396),r=n.n(l),o=n(2720),m=n(7581),c=n(641);const d=m.default.div.withConfig({displayName:"style__RecordsWrapper",componentId:"sc-gikyx1-0"})([".text-overflow-ui{display:flex;grid-gap:32px;"," justify-content:space-around;padding-top:44px;padding-bottom:48px;"," .count-item{display:flex;justify-content:center;flex-direction:column;align-items:center;"," "," ",' h2{font-family:"Caveat",cursive;font-size:90px;font-weight:',";margin:0;","}h3{font-size:",";font-weight:",";color:",";padding-top:21px;margin:0;","}}}.marque-slider{display:none;",' .card-wrapper{display:flex;grid-gap:24px;justify-content:center;align-items:center;text-align:center;margin-right:50px;.count-item{min-width:250px;max-width:100%;h2{font-family:"Caveat",cursive;font-size:90px;font-weight:',";margin:0;","}h3{font-size:",";font-weight:",";color:",";padding-top:21px;margin:0;","}}}}"],c.$.md`
+  display:none;
+    `,c.$.sm`
+      overflow: auto;
+        grid-gap: 16px;
+        justify-content: start;
+      `,c.$.xl`
+        min-width: auto;
+        `,c.$.sm`
+      min-width: 264px;
+      `,c.$.xs`
+      min-width: 181px;
+      `,(e=>{let{theme:t}=e;return t.fontWeight.bold}),c.$.sm`
+          font-size: 50px;
+        `,(e=>{let{theme:t}=e;return t.fonts.headingH2}),(e=>{let{theme:t}=e;return t.fontWeight.medium}),(e=>{let{theme:t}=e;return t.colors.primaryColor}),c.$.sm`
+          padding-top:8px;
+          font-size: ${e=>{let{theme:t}=e;return t.fonts.baseFontSize}};
+        `,c.$.md`
+  display:block;
+  margin: 76px 0px;
+    `,(e=>{let{theme:t}=e;return t.fontWeight.bold}),c.$.sm`
+          font-size: 50px;
+        `,(e=>{let{theme:t}=e;return t.fonts.headingH2}),(e=>{let{theme:t}=e;return t.fontWeight.medium}),(e=>{let{theme:t}=e;return t.colors.primaryColor}),c.$.sm`
+          padding-top:8px;
+          font-size: ${e=>{let{theme:t}=e;return t.fonts.baseFontSize}};
+        `),s=(0,a.lazy)((()=>n.e(223).then(n.bind(n,7700))));var p=()=>{const e=[{count:"100K+",text:"Accuracy Rate",delay:100,direction:"fadeIn"},{count:"1000+",text:"Total Received Batche’s",delay:500,direction:"fadeIn"},{count:"1000+",text:"Total Shipped Items",delay:700,direction:"fadeIn"}];return a.createElement(a.Suspense,{fallback:a.createElement(o.A,null)},a.createElement(d,null,a.createElement(s,null,a.createElement("div",{className:"text-overflow-ui"},e.map(((e,t)=>a.createElement(r(),{animateIn:e.direction,delay:e.delay},a.createElement("div",{key:t,className:"count-item"},a.createElement("h2",null,e.count),a.createElement("h3",null,e.text)))))),a.createElement("div",{className:"marque-slider"},a.createElement(a.Suspense,{fallback:null},a.createElement(i.A,{speed:50,direction:"right"},a.createElement("div",{className:"card-wrapper"},e.map(((e,t)=>a.createElement("div",{key:t,className:"count-item"},a.createElement("h2",null,e.count),a.createElement("h3",null,e.text)))))))))))}}}]);
+//# sourceMappingURL=534-8446876ddfb820d8d8c9.js.map
