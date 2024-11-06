6e897b358c9ac93f4e3eafe10990c81f18a634ff
kalash-qbatch <105213530+kalash-qbatch@users.noreply.github.com>
2024-11-06 18:24:15 +0500

    Updates

---
+++ 174-71978a81d2edf1e4aadc.js
@@ -0,0 +1,28 @@
+"use strict";(self.webpackChunk_3pl_landing_page=self.webpackChunk_3pl_landing_page||[]).push([[174],{8174:function(e,t,n){n.r(t),n.d(t,{default:function(){return x}});var r=n(6540),a=n(1605),l=n(8396),i=n.n(l),o=n.p+"static/demo-image-29710fe9481159b10971f5de32d7efe2.svg",m=n(1352),p=n(5729),s=n(2720),d=n(7581),c=n(641);const u=d.default.div.withConfig({displayName:"style__DemoWrapper",componentId:"sc-1atcjec-0"})(["margin-bottom:99px;"," .select-wrapper{display:flex;flex-direction:column;grid-row-gap:8px;label{font-size:16px;font-weight:",";line-height:24px;text-align:left;}select{padding:7.5px 12px;border-radius:8px;border:0.5px solid ",";padding:11px 12px 11px 12px;border-radius:8px;font-size:17px;font-weight:",";line-height:18px;text-align:left;color:",";cursor:pointer;}}.first-item{box-shadow:0px 1px 32px 0px #7777771A;border-radius:0px 40px 40px 0px;padding:71px 40px 71px 100px;"," "," ","}.demo-wrapper{display:flex;justify-content:space-between;"," .demo-items{width:100%;&:nth-child(2){display:flex;justify-content:center;align-items:center;}img.heading-icon{position:absolute;left:0;top:-30px;}}img{"," ","}form{margin-top:40px;.inputs-wrapper{display:grid;grid-template-columns:1fr 1fr;grid-gap:32px;align-items:center;","}}}button{display:flex;margin-top:48px;margin-left:auto;","}.heading{h2{font-size:48px;font-weight:",";line-height:64px;text-align:left;color:",";position:relative;","}}p{margin:0;font-size:",";font-weight:",";color:",";line-height:24px;text-align:left;",' span{font-family:"Caveat",cursive;}}'],c.$.sm`
+  margin-bottom: 24px;
+`,(e=>{let{theme:t}=e;return t.fontWeight.regular}),(e=>{let{theme:t}=e;return t.colors.roundStroke}),(e=>{let{theme:t}=e;return t.fontWeight.regular}),(e=>{let{theme:t}=e;return t.colors.bodyText}),c.$.sm`
+    box-shadow:none;
+      `,c.$.lg`
+    padding: 32px;
+      `,c.$.xs`
+    padding: 24px;
+      `,c.$.sm`
+    flex-direction:column;  
+      `,c.$.lg`
+      max-width: 70%;
+      `,c.$.sm`
+      max-width: 100%;
+      display: none;
+      `,c.$.sm`
+      grid-gap: 24px;
+      grid-template-columns: 1fr;
+      `,c.$.sm`
+  margin-top:24px;
+    `,(e=>{let{theme:t}=e;return t.fontWeight.bold}),(e=>{let{theme:t}=e;return t.colors.primaryColor}),c.$.sm`
+      font-size: ${e=>{let{theme:t}=e;return t.fonts.headingH1}};
+    line-height: 48px;
+    
+      `,(e=>{let{theme:t}=e;return t.fonts.headingH2}),(e=>{let{theme:t}=e;return t.fontWeight.regular}),(e=>{let{theme:t}=e;return t.colors.bodyTextVariant}),c.$.sm`
+  font-size: ${e=>{let{theme:t}=e;return t.fonts.baseFontSize}};
+`),g=(0,r.lazy)((()=>n.e(223).then(n.bind(n,8165)))),h=(0,r.lazy)((()=>n.e(105).then(n.bind(n,9105))));var x=()=>{const{0:e,1:t}=(0,r.useState)({username:"",email:"",phoneNumber:"",company:"",description:""}),{0:n,1:l}=(0,r.useState)({}),d=r=>{const{name:a,value:i}=r.target;t({...e,[a]:i}),l({...n,[a]:""})};return r.createElement(r.Suspense,{fallback:r.createElement(s.A,null)},r.createElement(u,{id:"demo"},r.createElement("div",{className:"demo-wrapper"},r.createElement(i(),{animateIn:"slideInLeft",delay:200,className:"demo-items first-item"},r.createElement("div",{className:"heading"},r.createElement("h2",null,"Book Free Demo ",r.createElement("img",{className:"heading-icon",src:m.A,title:"HeadingIcon",alt:"HeadingIcon"})),r.createElement("p",null,"We created software to simplify FBA inbound prep, making it simple,"),r.createElement("p",null,"easy, and accurate ",r.createElement("span",null,"— for you."))),r.createElement(a.A,{onSubmit:t=>{t.preventDefault();const n=(()=>{const t={};return e.username||(t.username="Username is required"),e.email?/\S+@\S+\.\S+/.test(e.email)||(t.email="Email address is invalid"):t.email="Email is required",e.phoneNumber||(t.phoneNumber="Phone Number is required"),e.company||(t.company="Company is required"),e.description||(t.description="Description is required"),t})();Object.keys(n).length>0?l(n):console.log("Submitted form:",e)}},r.createElement("div",{className:"inputs-wrapper"},[{controlId:"formUsername",label:"Username",name:"username",type:"text",placeholder:"Enter"},{controlId:"formEmail",label:"Email address",name:"email",type:"email",placeholder:"Enter"},{controlId:"formPhoneNumber",label:"Phone NO#",name:"phoneNumber",type:"number",placeholder:"Enter"},{controlId:"formCompany",label:"Company",name:"company",type:"text",placeholder:"Enter"}].map((t=>r.createElement(h,{key:t.controlId,controlId:t.controlId,label:t.label,name:t.name,type:t.type,placeholder:t.placeholder,helperText:t.helperText,value:e[t.name],onChange:d,error:n[t.name]})))),r.createElement(h,{as:"textarea",controlId:"formDescription",className:"mt-4 pt-1",label:"Description",name:"description",placeholder:"Enter",row:3,value:e.description,onChange:d,error:n.description}),r.createElement(g,{image:p.A,type:"submit"},"Start Demo"))),r.createElement(i(),{animateIn:"slideInRight",delay:200,className:"demo-items"},r.createElement("img",{src:o,title:"DemoImage",alt:"DemoImage"})))))}}}]);
+//# sourceMappingURL=174-71978a81d2edf1e4aadc.js.map
